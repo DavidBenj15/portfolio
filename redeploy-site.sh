3a3c60f7d6d3028d849c72e44633aa0ff0344ef9
David <davidnbenjamin15@gmail.com>
2025-10-20 13:50:13 -0400

    Build app on GH instead of VPS

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -e
 
 cd ~
 if [ ! -d portfolio ]; then
-	echo "Could not find directory '~/portfolio'"
-	echo "Cloning portfolio from github..."
-	git clone https://github.com/DavidBenj15/portfolio.git portfolio
+  echo "Could not find directory '~/portfolio'"
+  echo "Cloning portfolio from github..."
+  git clone https://github.com/DavidBenj15/portfolio.git portfolio
 fi
 
 echo "Changing into portfolio directory"
@@ -14,16 +15,22 @@ echo "Fetching latest changes from GitHub..."
 git fetch && git reset origin/main --hard
 
 if [ ! -f ".env" ]; then
-	echo "Error: could not find file '.env'. Exiting."
-	exit 1
+  echo "Error: could not find file '.env'. Exiting."
+  exit 1
 fi
 
 if [ ! -f "docker-compose.prod.yml" ]; then
-	echo "Error: could not find file 'docker-compose.prod.yml'. Exiting."
-	exit 1
+  echo "Error: could not find file 'docker-compose.prod.yml'. Exiting."
+  exit 1
 fi
 
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
+echo "Pulling images from registry..."
+docker compose -f docker-compose.prod.yml pull
+
+echo "Bringing services up..."
+docker compose -f docker-compose.prod.yml up -d
+
+echo "Cleaning up unused images..."
+docker image prune -f
 
 echo "Success! Portfolio has been successfully redeployed."
